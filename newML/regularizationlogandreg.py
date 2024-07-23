4b7881e03c7c3b804fe28b3c742be29e9f4882d8
Trevor Conger <149925363+trevcong@users.noreply.github.com>
2024-07-22 21:45:06 -0500

    new

---
+++ newML/regularizationlogandreg.py
@@ -0,0 +1,44 @@
+import numpy as np
+from matplotlib import widget
+import matplotlib.pyplot as plt
+from lab_utils_common import sigmoid
+np.set_printoptions(precision=8)
+
+def compute_cost_linear_reg(X, y, w, b, lambda_ = 1):
+    """
+    Computes the cost over all examples
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+    Returns:
+      total_cost (scalar):  cost 
+    """
+
+    m  = X.shape[0]
+    n  = len(w)
+    cost = 0.
+    for i in range(m):
+        f_wb_i = np.dot(X[i], w) + b                                   #(n,)(n,)=scalar, see np.dot
+        cost = cost + (f_wb_i - y[i])**2                               #scalar             
+    cost = cost / (2 * m)                                              #scalar  
+ 
+    reg_cost = 0
+    for j in range(n):
+        reg_cost += (w[j]**2)                                          #scalar
+    reg_cost = (lambda_/(2*m)) * reg_cost                              #scalar
+    
+    total_cost = cost + reg_cost                                       #scalar
+    return total_cost                                                  #scalar
+
+np.random.seed(1)
+X_tmp = np.random.rand(5,6)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1]).reshape(-1,)-0.5
+b_tmp = 0.5
+lambda_tmp = 0.7
+cost_tmp = compute_cost_linear_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
+
+print("Regularized cost:", cost_tmp)
